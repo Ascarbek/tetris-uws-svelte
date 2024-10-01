a219c8702a35456603999eebcbdf4374456639d7
ascarbek <ascarbek@gmail.com>
2024-10-01 17:33:12 +0500

    home key events

--- modules/frontend/src/pages/home/HomePage.svelte
+++ modules/frontend/src/pages/home/HomePage.svelte
@@ -1,7 +1,31 @@
 <script lang="ts">
   import Board from '$widgets/game/Board.svelte';
   import Toolbar from '$widgets/game/Toolbar.svelte';
+  import type { KeyboardEventHandler } from 'svelte/elements';
+
+  let pressed = false;
+
+  const onKeyDown: KeyboardEventHandler<Document> = (e) => {
+    if (e.code === 'ArrowLeft' && !pressed) {
+      pressed = true;
+      console.log('left');
+    }
+    if (e.code === 'ArrowRight' && !pressed) {
+      pressed = true;
+      console.log('right');
+    }
+    if (e.code === 'Space' && !pressed) {
+      pressed = true;
+      console.log('space');
+    }
+  };
+
+  const onKeyUp: KeyboardEventHandler<Document> = (e) => {
+    pressed = false;
+  };
 </script>
+
+<svelte:document on:keydown="{onKeyDown}" on:keyup="{onKeyUp}" />
 
 <div class="flex items-center justify-center h-full">
   <div class="flex flex-col items-center gap-8">
